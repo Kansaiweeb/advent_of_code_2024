7485807891c7131a70252ee1a823af3d936fa677
Robert Brons <robbrons35@gmail.com>
2024-12-02 19:49:21 +0500

    Day01b

--- day01/src/solution.rs
+++ day01/src/solution.rs
@@ -1,21 +1,27 @@
+use std::collections::HashMap;
 use std::str::FromStr;
 use util::read_input;
 
-pub fn solution(file_name: &str) -> String {
+pub fn solution(file_name: &str) -> (String, String) {
     let input = read_input(&format!("./day01/{}", file_name)).unwrap();
     let input = input.trim();
     let data = ParsedData::from_str(&input).unwrap();
-    data.total_distance().to_string()
+    (data.total_distance().to_string(), data.similarity().to_string())
 }
 
 struct ParsedData {
     left_list: Vec<u32>,
     right_list: Vec<u32>,
+    right_frequencies: HashMap<u32, u32>,
 }
 
 impl ParsedData {
     pub fn total_distance(&self) -> u32 {
         self.left_list.iter().zip(self.right_list.iter()).map(|(&l, &r)| r.abs_diff(l)).sum()
+    }
+
+    pub fn similarity(&self) -> u32 {
+        self.left_list.iter().map(|element| element * self.right_frequencies.get(element).unwrap_or(&0)).sum()
     }
 }
 
@@ -32,12 +38,17 @@ impl FromStr for ParsedData {
                 (left, right)
             })
             .collect();
+        let mut right_frequencies: HashMap<u32, u32>= HashMap::new();
+        for value in &right_list {
+            *right_frequencies.entry(*value).or_default() +=1;
+        }
         left_list.sort();
         right_list.sort();
 
         Ok(ParsedData {
             left_list,
             right_list,
+            right_frequencies
         })
     }
 }
--- src/solutions.rs
+++ src/solutions.rs
@@ -4,10 +4,10 @@ pub fn solve_all() {
 
 pub(crate) fn solve() {
     let day01 = day01::solution::solution("input.txt");
-    println!("day01 {}", day01);
+    println!("day01 a: {}, b: {}", day01.0, day01.1);
 }
 
 pub(crate) fn solve_examples() {
     let day01 = day01::solution::solution("example_input.txt");
-    println!("day01 {}", day01);
+    println!("day01 a: {}, b: {}", day01.0, day01.1);
 }
